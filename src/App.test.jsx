a3d60fb41911da51ebf4e3bc59635038642165e0
허현진/연구원/ACP Unit <hyeonjin.heo@lge.com>
2025-02-28 17:12:52 +0900

    [3] Add annotations

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  render(<App />); // request to render the App component
+  const headingElement = screen.getByText(/learn react/i); // Find element by display text (regular expression - flexible search)
+  expect(headingElement).toBeInTheDocument(); // expect the element to be in the document
 });
